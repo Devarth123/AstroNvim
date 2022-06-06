85ab51da5e7ceb9bef5e3a81bc49d36812db30bd
Devarth123 <devarthsarayan485@gmail.com>
2022-06-06 13:12:53 +0530

    plugins

---
+++ lua/user/plugins/init.lua
@@ -0,0 +1,22 @@
+return {
+  { "andweeb/presence.nvim", "Pocco81/AutoSave.nvim" },
+  {
+    "ray-x/lsp_signature.nvim",
+    event = "BufRead",
+    config = function()
+      require("lsp_signature").setup()
+    end,
+  },
+}
+
+-- return {
+--   { "andweeb/presence.nvim" },
+--   {
+--     "ray-x/lsp_signature.nvim",
+--     event = "BufRead",
+--     config = function()
+--       require("lsp_signature").setup()
+--     end,
+--   },
+-- }
+--
